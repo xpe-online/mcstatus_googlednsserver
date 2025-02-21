79c9d7151981da99be4d47f193047c62a171ce33
xpe-online <xpecnh2n@gmail.com>
2025-02-21 21:55:15 +0800

    Set the DNS server to Google DNS Server

--- mcstatus/dns.py
+++ mcstatus/dns.py
@@ -10,49 +10,27 @@ from dns.rdtypes.IN.SRV import SRV as SRVRecordAnswer  # noqa: N811 # constant i
 
 
 def resolve_a_record(hostname: str, lifetime: float | None = None) -> str:
-    """Perform a DNS resolution for an A record to given hostname
-
-    :param hostname: The address to resolve for.
-    :return: The resolved IP address from the A record
-    :raises dns.exception.DNSException:
-        One of the exceptions possibly raised by :func:`dns.resolver.resolve`.
-        Most notably this will be :exc:`dns.exception.Timeout`, :exc:`dns.resolver.NXDOMAIN`
-        and :exc:`dns.resolver.NoAnswer`
-    """
-    answers = dns.resolver.resolve(hostname, RdataType.A, lifetime=lifetime, search=True)
-    # There should only be one answer here, though in case the server
-    # does actually point to multiple IPs, we just pick the first one
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # 使用 Google 的 DNS 服务器
+    answers = resolver.resolve(hostname, RdataType.A, lifetime=lifetime)
     answer = cast(ARecordAnswer, answers[0])
     ip = str(answer).rstrip(".")
     return ip
 
 
 async def async_resolve_a_record(hostname: str, lifetime: float | None = None) -> str:
-    """Asynchronous alternative to :func:`.resolve_a_record`.
-
-    For more details, check it.
-    """
-    answers = await dns.asyncresolver.resolve(hostname, RdataType.A, lifetime=lifetime, search=True)
-    # There should only be one answer here, though in case the server
-    # does actually point to multiple IPs, we just pick the first one
+    resolver = dns.asyncresolver.Resolver()
+    resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # 使用 Google 的 DNS 服务器
+    answers = await resolver.resolve(hostname, RdataType.A, lifetime=lifetime)
     answer = cast(ARecordAnswer, answers[0])
     ip = str(answer).rstrip(".")
     return ip
 
 
 def resolve_srv_record(query_name: str, lifetime: float | None = None) -> tuple[str, int]:
-    """Perform a DNS resolution for SRV record pointing to the Java Server.
-
-    :param query_name: The address to resolve for.
-    :return: A tuple of host string and port number
-    :raises dns.exception.DNSException:
-        One of the exceptions possibly raised by :func:`dns.resolver.resolve`.
-        Most notably this will be :exc:`dns.exception.Timeout`, :exc:`dns.resolver.NXDOMAIN`
-        and :exc:`dns.resolver.NoAnswer`
-    """
-    answers = dns.resolver.resolve(query_name, RdataType.SRV, lifetime=lifetime, search=True)
-    # There should only be one answer here, though in case the server
-    # does actually point to multiple IPs, we just pick the first one
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # 使用 Google 的 DNS 服务器
+    answers = resolver.resolve(query_name, RdataType.SRV, lifetime=lifetime)
     answer = cast(SRVRecordAnswer, answers[0])
     host = str(answer.target).rstrip(".")
     port = int(answer.port)
@@ -60,18 +38,13 @@ def resolve_srv_record(query_name: str, lifetime: float | None = None) -> tuple[
 
 
 async def async_resolve_srv_record(query_name: str, lifetime: float | None = None) -> tuple[str, int]:
-    """Asynchronous alternative to :func:`.resolve_srv_record`.
-
-    For more details, check it.
-    """
-    answers = await dns.asyncresolver.resolve(query_name, RdataType.SRV, lifetime=lifetime, search=True)
-    # There should only be one answer here, though in case the server
-    # does actually point to multiple IPs, we just pick the first one
+    resolver = dns.asyncresolver.Resolver()
+    resolver.nameservers = ['8.8.8.8', '8.8.4.4']  # 使用 Google 的 DNS 服务器
+    answers = await resolver.resolve(query_name, RdataType.SRV, lifetime=lifetime)
     answer = cast(SRVRecordAnswer, answers[0])
     host = str(answer.target).rstrip(".")
     port = int(answer.port)
     return host, port
-
 
 def resolve_mc_srv(hostname: str, lifetime: float | None = None) -> tuple[str, int]:
     """Resolve SRV record for a minecraft server on given hostname.
